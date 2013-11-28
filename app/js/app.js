82a41a608c70831d24ef5b2388093f32d12a1051
Nelson Rodrigues <nelson@fullfront.pt>
2013-11-28 20:31:09 +0000

    Call ngRoute module on init

--- app/js/app.js
+++ app/js/app.js
@@ -2,13 +2,12 @@
 window.config = loadConfig('config.json');
 
 // Initialize Application
-app = angular.module('app', [])
-	.config(function($routeProvider) {
-		$routeProvider
-			.when('/new', {templateUrl:'./views/new.html', controller: 'NewController'})
-			.when('/edit/:id', {templateUrl: './views/edit.html', controller: 'EditController'})
-			.otherwise({redirectTo: '/new'});
-	});
+app = angular.module('app', ['ngRoute']).config(function($routeProvider) {
+	$routeProvider
+		.when('/new', {templateUrl:'./views/new.html', controller: 'NewController'})
+		.when('/edit/:id', {templateUrl: './views/edit.html', controller: 'EditController'})
+		.otherwise({redirectTo: '/new'});
+});
 
 // Prevent application flash
 preventFlash();
