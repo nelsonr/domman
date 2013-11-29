b6f57dcd5453b1efdcaaeacae6271281e2dd1b62
Nelson Rodrigues <nelson@fullfront.pt>
2013-11-29 17:55:06 +0000

    Refactored and added new tests

--- tests/test.js
+++ tests/test.js
@@ -1,16 +1,37 @@
 var assert = require('assert');
 
 describe('vhostsParser', function() {
+	var vhostsParser = require('../app/node_modules/apache-vhosts-parser.js');
+
 	describe('parse()', function() {
-		var vhostsParser = require('../app/node_modules/apache-vhosts-parser.js');
 		var expected = [
-			{id: 0, ServerName: 'localhost', ServerAdmin: 'admin@localhost', DocumentRoot: '"C:\\apache-www\\"'},
-			{id: 1, ServerName: 'domainA.dev', DocumentRoot: '"C:\\apache-www\\domainA"'},
-			{id: 2, ServerName: 'domainB.dev', DocumentRoot: '"C:\\apache-www\\domainB"'}
+			{id: 0, ServerName: 'localhost', ServerAdmin: 'admin@localhost', DocumentRoot: 'C:\\apache-www\\'},
+			{id: 1, ServerName: 'domainA.dev', DocumentRoot: 'C:\\apache-www\\domainA'},
+			{id: 2, ServerName: 'domainB.dev', DocumentRoot: 'C:\\apache-www\\domainB'}
 		];
 
 		it('should return list of virtual host objects', function() {
 			assert.deepEqual(expected, vhostsParser.parse('assets/httpd-vhosts.conf'));
 		});
 	});
+});
+
+describe('tools', function() {
+	var tools = require('../app/js/tools.js');
+
+    describe('compareObjects()', function() {
+	    it("should return false if two objects don't match", function() {
+		    var objA = {a: 1, b: 2},
+			    objB = {a: 1, b: 3};
+
+	        assert.equal(false, tools.compareObjects(objA, objB));
+	    });
+
+	    it('should return true if two objects match', function() {
+		    var objA = {a: 1, b: 2},
+			    objB = {a: 1, b: 2};
+
+	        assert.equal(true, tools.compareObjects(objA, objB));
+	    });
+    });
 });
